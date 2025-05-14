c1dd7aaab8481e2a2451df8927bbb3a35b26204f
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-05-14 07:52:42 +0300

    TODO 1 NOT COMPLETE

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,33 @@
 import UserList from "../components/UserList"
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import { displayNameInPopUp } from "./Search";
 
 
 function App() {
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-      fetch("https://jsonplaceholder.typicode.com/users/")
+
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/`)
       .then(response => response.json())
       .then(data => {
         setUsers(data)
       })
-    }, [])
+  }, [clicked])
 
-    return (
-        <>
-          <UserList users={users}/>
-        </>
-    )
+  return (
+    <>
+      <UserList users={users} />
+    </>
+  )
 }
 
 export default App
+
+// Todos
+// 1. Implement a function that will handle getting a random user when the Button **Get Random User** is clicked.
+//  - This can be achieved by tracking the url in state and updating the parameter using the numbers between 1 and 10
+// 2. When the search param is clicked, render the single user card, otherwise, render the entire users list
+// 3. Use the url as the dependency inside the useEffect hook, such that it loads everytime the URL changes.
+// 4. Add a button **Get All Users** that when clicked will fetch all the 10 users and display them.
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,21 @@
 export default function Search() {
+    function displayNameInPopUp() {
+        // Generate a random number btn 1 and 10
+        const randomNumber = Math.floor(Math.random() * 10);
+        console.log(randomNumber);
+        // Find a way to add the number to the URL 
+
+
+    }
+
     return (
         <div className="search">
-            <input className="search-number" type="button" value="Get Random User"/>
+            <input className="search-number" type="button" value="Get Random User" onClick={displayNameInPopUp} />
         </div>
     )
 }
+
+export { displayNameInPopUp }
+
+// 1. Implement a function that will handle getting a random user when the Button **Get Random User** is clicked.
+//  - This can be achieved by tracking the url in state and updating the parameter using the numbers between 1 and 10
